18a24674704b6ce2e831f55aa77adfae94c1ba20
Ramon Larivoir <rlarivoir@hotmail.com>
2019-09-07 16:13:50 -0300

    finalizando atividade

--- controllers.js
+++ controllers.js
@@ -1,11 +1,36 @@
+var qs = require("querystring");
+
 exports.oculto = function oculto(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<h1>oculto</h1>");
-    res.end();
+    var queryData = '';
+    req.on('data', function (data) {
+        queryData += data;
+    });
+
+    req.on('end', function () {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        var datas = qs.parse(queryData);
+        if (datas.password == '54321') {
+            res.write("<h1>Autorizado!</h1>");
+        }
+        else {
+            res.write(`
+            <html>
+            <head>
+                <meta charset="utf-8" />
+                <title>Index</title>
+            </head>
+            <body>
+                <h1>Não autorizado!</h1>
+                <a href='index.html'>Voltar</a>
+            </body>
+            `);
+            res.end();
+        }
+    });
 }
 
 exports.index = function index(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"});
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.write(`
     <html>
     <head>
@@ -15,7 +40,7 @@ exports.index = function index(req, res) {
     <body>
     <form method='post' action='oculto.html'>
     <label>Senha:</label>
-    <input type='text' placehold='senha'>
+    <input type='text' placehold='senha' name='password'>
     <button type='submit'>Enviar</button>
     </form>
     `);
@@ -23,7 +48,7 @@ exports.index = function index(req, res) {
 }
 
 exports.notfound = function (req, res) {
-    res.writeHead(404, {"Content-Type": "text/html"});
+    res.writeHead(404, { "Content-Type": "text/html" });
     res.write(`<h1>Página não encontrada: ${req.url}</h1>`);
     res.end();
 }
