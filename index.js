d563080c62a7f598d9492456c2696ef89937432d
Ramon Larivoir <rlarivoir@hotmail.com>
2019-09-07 15:42:05 -0300

    adiciona rota para oculto

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const controllers = require("./controllers");
 const rotas = {};
 rotas["/"] = controllers.index;
 rotas["/index.html"] = controllers.index;
+rotas["/oculto.html"] = controllers.oculto;
 rotas["404"] = controllers.notfound;
 
 server.start(router.route, rotas);
